8328f2182dae87e8d2aa7bd9a923a0b0367196e0
sako44 <radwansrank.mdbf21@iste.edu.tr>
2025-02-15 18:04:13 +0300

    lastFix

--- components/Services.tsx
+++ components/Services.tsx
@@ -5,12 +5,18 @@ import satin from "@/public/photos/satin.png";
 import Container from "./Container";
 import { Timeline } from "@/components/ui/timeline";
 import Images from "./images";
+import MotionDiv from "./MotionDiv";
+import { animatedTimelineTwo } from "@/utils/motionObjects";
 
 const data = [
   {
     title: "1. Adım",
     content: (
-      <div>
+      <MotionDiv
+        {...animatedTimelineTwo}
+        initial={{ ...animatedTimelineTwo.initial, x: 30 }}
+        whileInView={{ ...animatedTimelineTwo.whileInView, x: 0 }}
+      >
         <p className="text-neutral-800 dark:text-neutral-200 text-lg md:text-2xl font-normal mb-8">
           Parsel ve ürün tanımlama.
         </p>
@@ -19,13 +25,17 @@ const data = [
           addClass="w-full  h-[250px] md:h-[170px] md:w-[330px] lg:w-[480px] lg:h-[250px] xl:w-[600px] xl:h-[250px] 2xl:w-[750px] 2xl:h-[300px] "
           clas=" object-contain object-center  "
         />
-      </div>
+      </MotionDiv>
     ),
   },
   {
     title: "2. Adım",
     content: (
-      <div>
+      <MotionDiv
+        {...animatedTimelineTwo}
+        initial={{ ...animatedTimelineTwo.initial, x: 30 }}
+        whileInView={{ ...animatedTimelineTwo.whileInView, x: 0 }}
+      >
         <p className="text-neutral-800 dark:text-neutral-200 text-lg md:text-2xl font-normal mb-8">
           İlaçlama ve gübreleme planlarını yapma.
         </p>
@@ -34,13 +44,17 @@ const data = [
           addClass="w-full  h-[250px] md:h-[170px] md:w-[330px] lg:w-[480px] lg:h-[150px] xl:w-[600px] 2xl:w-[750px] 2xl:h-[300px] "
           clas=" object-contain object-center  "
         />
-      </div>
+      </MotionDiv>
     ),
   },
   {
     title: "3. Adım",
     content: (
-      <div>
+      <MotionDiv
+        {...animatedTimelineTwo}
+        initial={{ ...animatedTimelineTwo.initial, x: 30 }}
+        whileInView={{ ...animatedTimelineTwo.whileInView, x: 0 }}
+      >
         <p className="text-neutral-800 dark:text-neutral-200 text-lg md:text-2xl font-normal mb-4">
           Hasat ve satın alma süreçlerini takip etme.
         </p>
@@ -49,13 +63,17 @@ const data = [
           addClass=" w-full  h-[250px] md:h-[170px] md:w-[330px] lg:w-[480px] lg:h-[150px] xl:w-[600px]  2xl:w-[750px] 2xl:h-[300px] "
           clas=" object-contain object-center  "
         />
-      </div>
+      </MotionDiv>
     ),
   },
   {
     title: "4. Adım",
     content: (
-      <div>
+      <MotionDiv
+        {...animatedTimelineTwo}
+        initial={{ ...animatedTimelineTwo.initial, x: 30 }}
+        whileInView={{ ...animatedTimelineTwo.whileInView, x: 0 }}
+      >
         <p className="text-neutral-800 dark:text-neutral-200 text-lg md:text-2xl font-normal mb-4">
           Anlık bildirimlerle yönetim.
         </p>
@@ -64,7 +82,7 @@ const data = [
           addClass="w-full  h-[250px] md:h-[170px] md:w-[330px] lg:w-[480px] lg:h-[150px] xl:w-[600px] xl:h-[200px] 2xl:w-[750px] 2xl:h-[300px] "
           clas=" object-contain object-center  "
         />
-      </div>
+      </MotionDiv>
     ),
   },
 ];
